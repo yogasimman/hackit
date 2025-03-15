73cd1df526aae8a81b4c4c1d46d1e7d5c05eb6b2
manishm004 <manish.muru07@gmail.com>
2025-03-15 10:39:57 +0530

    Updated home page

---
+++ stock-simulator-backend/public/js/script.js
@@ -0,0 +1,8 @@
+// public/js/script.js
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  });
+  
